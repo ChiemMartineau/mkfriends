ecffea58b8e467648c46445b0a162f440603037a
Mathieu Guimond <mathguimond@gmail.com>
2026-01-18 08:48:40 -0500

    Fix

--- src/app/(authenticated)/choose-group/page.tsx
+++ src/app/(authenticated)/choose-group/page.tsx
@@ -8,12 +8,12 @@ export default async function ChooseGroup() {
   const session = await auth0.getSession();
   if (!session?.user.email) redirect("/");
 
-  await upsertUser({
+  const user = await upsertUser({
     email: session.user.email,
     name: session.user.name,
     profilePicture: session.user.picture,
   });
-  if (session.user.group) redirect("/home");
+  if (user?.group) redirect("/home");
 
   const allGroups = await getAllGroups();
   const plainGroups = allGroups.map((group) => ({
